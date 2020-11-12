2f32e0d9b9b2b8f409e34b002d3f43435c2d5ab2
hlopes <hugo.lopes@present-technologies.com>
2020-11-12 12:22:56 +0000

    Remove unused icons

--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -7,10 +7,6 @@ const Header = () => {
             <Head>
                 <title>Quiz Game</title>
                 <link rel="icon" href="/favicon.ico" />
-                <link
-                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                    rel="stylesheet"
-                />
             </Head>
         </header>
     );

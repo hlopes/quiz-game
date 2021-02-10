246df34729a7efeba0ab0980d9bec696dbc22577
Hugo Lopes <hugo.rodrigues.lopes@gmail.com>
2021-02-10 19:05:02 +0000

    chore: fix minor issues

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,18 +2,25 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
-const options = {
-    // Configure one or more authentication providers
-    providers: [
-        Providers.Google({
-            clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID,
-            clientSecret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET,
-        }),
+const providers = [
+    Providers.Google({
+        clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID,
+        clientSecret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET,
+    }),
+];
+
+if (process.env.FACEBOOK_CLIENT_ID) {
+    providers.push(
         Providers.Facebook({
             clientId: process.env.FACEBOOK_CLIENT_ID,
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        }),
-    ],
+        })
+    );
+}
+
+const options = {
+    // Configure one or more authentication providers
+    providers,
     // @link https://next-auth.js.org/configuration/databases
     database: process.env.NEXTAUTH_DATABASE_URL,
 };
